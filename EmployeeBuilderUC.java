6bbc7d43f901479d15f76f04ec302223b00abc0d
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-07 11:21:20 +0530

    ADDED : Employee Wages Total Hours & Max Days..

--- EmployeeBuilderUC.java
+++ EmployeeBuilderUC.java
@@ -5,18 +5,19 @@ public class EmployeeBuilderUC
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String ars[])
 	{
 
 		//Variables
 		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
 
-		for (int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
-
+			totalWorkingDays++;
 			//Computation
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 
@@ -33,11 +34,10 @@ public class EmployeeBuilderUC
 					empHrs=0;
 				break;
 			}
-			empWage=empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage +=empWage;
-			System.out.println("Emp Wage :"+empWage);
+			totalEmpHrs +=empHrs;
+			System.out.println("Day# :"+totalWorkingDays+"Emp Hrs: "+empHrs);
 		}
-
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("total Emp Wage : "+totalEmpWage);
 	}
 }
